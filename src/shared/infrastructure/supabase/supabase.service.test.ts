f9ea4de8add1d72ce14a15d695345a2bcbdfcbe7
William Morales <william.rolando88@hotmail.com>
2026-06-03 23:19:10 -0500

    Update types

--- src/shared/infrastructure/supabase/supabase.service.test.ts
+++ src/shared/infrastructure/supabase/supabase.service.test.ts
@@ -1,5 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
 import { SupabaseService } from './supabase.service';
 
 jest.mock('@supabase/supabase-js', () => ({
